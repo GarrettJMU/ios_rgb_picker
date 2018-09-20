28122599f4aeb79475067e84c2eb5669455720d1
Garrett Hughes <>
2018-09-19 19:39:51 -0700

    working rev 1

--- rgb_picker/AppDelegate.swift
+++ rgb_picker/AppDelegate.swift
@@ -15,13 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        let viewController = self.window?.rootViewController as? ViewController
+        
+        let defaults = UserDefaults.standard
+        defaults.setValue(viewController!.redColor, forKey: "red")
+        defaults.setValue(viewController!.greenColor, forKey: "green")
+        defaults.setValue(viewController!.blueColor, forKey: "blue")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -34,11 +38,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
+
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
 
--- rgb_picker/ViewController.swift
+++ rgb_picker/ViewController.swift
@@ -9,17 +9,109 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var redInput: UITextField!
+    @IBOutlet weak var greenInput: UITextField!
+    @IBOutlet weak var blueInput: UITextField!
+    @IBOutlet weak var resultingColor: UILabel!
+    @IBOutlet weak var inputButton: UIButton!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    var redColor: Float = 0
+    var greenColor: Float = 0
+    var blueColor: Float = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let oldRedColor = UserDefaults.standard.value(forKey: "red")
+        let oldGreenColor = UserDefaults.standard.value(forKey: "green")
+        let oldBlueColor = UserDefaults.standard.value(forKey: "blue")
+        
+        if ((oldRedColor as? Float) != nil) && ((oldGreenColor as? Float) != nil) && ((oldBlueColor as? Float) != nil) {
+            
+            let floatedRed = oldRedColor as! Float
+            let floatedGreen = oldGreenColor as! Float
+            let floatedBlue = oldBlueColor as! Float
+            changeDisplayColor(red: floatedRed, green: floatedGreen, blue: floatedBlue)
+            updateInputsAndSliders(red: floatedRed, green: floatedGreen, blue: floatedBlue)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func redInputAction(_ sender: UITextField) {
+        redColor = Float(redInput.text!)!
+    }
+    @IBAction func greenInputAction(_ sender: UITextField) {
+        greenColor = Float(greenInput.text!)!
+    }
+    @IBAction func blueInputAction(_ sender: UITextField) {
+        blueColor = Float(blueInput.text!)!
+    }
+    @IBAction func redSliderInput(_ sender: Any) {
+        redInput.text = String(Int(redSlider.value))
+    }
+    @IBAction func greenSliderInput(_ sender: Any) {
+        greenInput.text = String(Int(greenSlider.value))
+    }
+    @IBAction func blueSliderInput(_ sender: Any) {
+        blueInput.text = String(Int(blueSlider.value))
+    }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        updateColors()
+        updateSlidersOnSubmit()
+        dismissKeyboard()
+        changeDisplayColor(red: redColor, green: greenColor, blue: blueColor)
+    }
+    
+    func updateColors() {
+        redColor = Float(redInput.text!)!
+        greenColor = Float(greenInput.text!)!
+        blueColor = Float(blueInput.text!)!
+    }
+    
+    func updateSlidersOnSubmit()    {
+        redSlider.value = Float(redInput.text!)!
+        greenSlider.value = Float(greenInput.text!)!
+        blueSlider.value = Float(blueInput.text!)!
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let position = touch.location(in: self.view)
+            resultingColor.center = CGPoint(x: position.x, y: position.y)
+        }
+    }
+    
+    func updateInputsAndSliders(red: Float, green: Float, blue: Float)    {
+        redSlider.value = red
+        greenSlider.value = green
+        blueSlider.value = blue
+        
+        redInput.text = String(Int(red))
+        greenInput.text = String(Int(green))
+        blueInput.text = String(Int(blue))
+    }
+    
+    func dismissKeyboard()  {
+        view.endEditing(true)
+    }
+    
+    func changeDisplayColor(red: Float, green: Float, blue: Float)   {
+        let red = CGFloat(red/255)
+        let green = CGFloat(green/255)
+        let blue  = CGFloat(blue/255)
+        resultingColor.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
-
